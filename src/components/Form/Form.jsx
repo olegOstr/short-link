b86d1ed5d67e5b142e86b2ed99c3e04e3706bf92
Oleg <olego250@gmail.com>
2022-01-20 10:01:43 +0200

    add: redux config (store, slice, AsyncThunk)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,56 @@
+import {useForm} from 'react-hook-form';
 import {Button} from 'components/Button';
 
 import classes from './Form.module.scss';
 
 const Form = () => {
-  return (
-    <section className={classes.section}>
-      <div className="container">
-        <form
-          className={classes.form}
-          autoComplete='off'
-        >
-          <input
-            type="url"
-            placeholder="Shorten a link here..."
-            className={classes.input}
-          />
-          <Button
-            variant="square"
-            type="submit"
-            size="medium"
-          >Shorten it!</Button>
-        </form>
-      </div>
-    </section>
-  )
+
+    const {
+        register,
+        formState: {errors},
+        handleSubmit,
+        reset
+    } = useForm({mode: 'onSubmit'})
+
+    const onSubmit = (data) => {
+        console.log(data)
+        reset();
+    }
+
+    return (
+        <section className={classes.section}>
+            <div className="container">
+                <form
+                    className={classes.form}
+                    autoComplete='off'
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <input
+                        type="url"
+                        placeholder="Shorten a link here..."
+                        className={classes.input}
+                        {...register('Url', {
+                            required: 'Please add a link',
+                            pattern: {
+                                value: /(http(s)?:\/\/.).?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+                                message: 'Please enter a valid url',
+                            },
+                        })}
+                    />
+                    <Button
+                        variant="square"
+                        type="submit"
+                        size="medium"
+                    >Shorten it!</Button>
+                    {errors.Url && (
+                        <div className={classes.error}>
+                            {errors.Url.message}
+                        </div>
+                    )}
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export {Form};
---
+++ src/store/slice/linkSlice.js
@@ -0,0 +1,42 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import {API_BASE_URL} from '../../configApi';
+
+export const createShortLink = createAsyncThunk(
+    'links/createShortLink',
+    async (url) => {
+        const response = await fetch(API_BASE_URL + url, {method: 'POST'})
+        return await response.json()
+    }
+)
+
+const
+    initialState = {
+        items: [],
+        loading: 'idle',
+    }
+
+const linkSlice = createSlice({
+    name: 'links',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [createShortLink.rejected]: (state) => {
+            state.loading = 'rejected'
+        },
+        [createShortLink.pending]: (state) => {
+            state.loading = 'loading'
+        },
+        [createShortLink.fulfilled]: (state, action) => {
+            const {ok, result} = action.payload
+
+            if (ok) {
+                state.items.push(result)
+                state.loading = 'idle'
+            } else {
+                state.loading = 'error'
+            }
+        },
+    }
+})
+
+export default linkSlice.reducer
