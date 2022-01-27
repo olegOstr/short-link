8de9f328571ddd3360423119043baea7044909bd
Oleg <olego250@gmail.com>
2022-01-27 13:43:12 +0200

    add: custom Hook - Change theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import {useState} from 'react'
-import {IoMenu} from 'react-icons/io5';
-import {motion, AnimatePresence} from "framer-motion";
+import {IoMenu, IoMoonOutline, IoSunny} from 'react-icons/io5';
+import {motion, AnimatePresence} from 'framer-motion';
 import useMatchMedia from 'use-match-media-hook';
 
 import classes from './Header.module.scss';
-import Logo from 'images/logo.svg';
+import {ReactComponent as Logo} from 'images/logo.svg';
 import {Menu} from './Menu';
 
 import {Modal} from 'components/Modal';
 import {SignUp} from 'components/SignUp';
 import {Login} from 'components/SignUp';
+import {useTheme} from '../../hooks/useTheme';
 
 const menuItems = [
     {
@@ -29,7 +30,7 @@ const menuItems = [
 const queries = [
     '(max-width: 766px)',
     '(min-width: 767px)'
-  ]
+]
 
 export const Header = () => {
     const [mobile] = useMatchMedia(queries);
@@ -42,40 +43,55 @@ export const Header = () => {
     const openLogin = () => setLoginOpen(true);
     const closeLogin = () => setLoginOpen(false);
 
+    const {theme, setTheme} = useTheme();
+
+    const handleThemeChange = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
     return (
         <>
-        <header className={`${classes.header} container`}>
-            <img src={Logo} alt="logo" className={classes.logo}/>
-            {mobile ? (
-             <IoMenu
-                className={classes.burger}
-                onClick={() => setShowMobileMenu(!showMobileMenu)}
-            />
-             ) : (
-                <Menu links={menuItems} handleSignup={openSignup} handleLogin={openLogin}/>
-            )}
-            <AnimatePresence>
-                {
-                    showMobileMenu && mobile && (
-                        <motion.div
-                            initial={{ opacity: 0, height: 0 }}
-                            animate={{ opacity: 1, height: 'auto' }}
-                            exit={{ opacity: 0, height: 0 }}
-                            className={classes.mobileMenu}
-                        >
-                            <Menu links={menuItems} handleSignup={openSignup} handleLogin={openLogin} />
-                        </motion.div>
-                    )
-                }
-            </AnimatePresence>
-        </header>
+            <header className={`${classes.header} container`}>
+                <div className={classes.logo}>
+                    <Logo/>
+                </div>
 
-        <Modal open={isSignupOpen} title="Sign Up" handleClose={closeSignup}>
-            <SignUp closeModal={closeSignup} />
-        </Modal>
-        <Modal open={isLoginOpen} title="Sign In" handleClose={closeLogin}>
-            <Login closeModal={closeLogin} />
-        </Modal>
+                {mobile ? (
+                    <IoMenu
+                        className={classes.burger}
+                        onClick={() => setShowMobileMenu(!showMobileMenu)}
+                    />
+                ) : (
+                    <Menu links={menuItems} handleSignup={openSignup} handleLogin={openLogin}/>
+                )}
+                <AnimatePresence>
+                    {
+                        showMobileMenu && mobile && (
+                            <motion.div
+                                initial={{opacity: 0, height: 0}}
+                                animate={{opacity: 1, height: 'auto'}}
+                                exit={{opacity: 0, height: 0}}
+                                className={classes.mobileMenu}
+                            >
+                                <Menu links={menuItems} handleSignup={openSignup} handleLogin={openLogin}/>
+                            </motion.div>
+                        )
+
+                    }
+                </AnimatePresence>
+
+                <button className={classes.themeBtn} onClick={handleThemeChange}>
+                    <IoSunny className={classes.theme}/>
+                </button>
+
+            </header>
+
+            <Modal open={isSignupOpen} title="Sign Up" handleClose={closeSignup}>
+                <SignUp closeModal={closeSignup}/>
+            </Modal>
+            <Modal open={isLoginOpen} title="Sign In" handleClose={closeLogin}>
+                <Login closeModal={closeLogin}/>
+            </Modal>
         </>
     )
 }
