4d3e32526ad027b893782214ed22fb5800ad4a3a
Oleg <olego250@gmail.com>
2022-01-27 14:36:08 +0200

    add: system theme sync

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,7 +1,10 @@
 import React, {useLayoutEffect, useState} from 'react';
 
+const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches
+const defaultTheme = isDarkTheme ? 'dark' : 'light'
+
 export const useTheme = () => {
-    const [theme, setTheme] = useState(localStorage.getItem('app-theme') || 'light')
+    const [theme, setTheme] = useState(localStorage.getItem('app-theme') || defaultTheme)
 
     useLayoutEffect(() => {
         localStorage.setItem('app-theme', theme)
